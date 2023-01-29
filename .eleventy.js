09b376d4558db3c8c4ef917b7b7efd5bcc89f12b
Cameron MacFarland <distantcam@gmail.com>
2023-01-29 17:49:43 +0800

    Upgrade 11ty to v2

--- .eleventy.js
+++ .eleventy.js
@@ -5,8 +5,6 @@ module.exports = function(eleventyConfig) {
 
   if (process.env.ELEVENTY_PRODUCTION) {
     eleventyConfig.addTransform("htmlmin", htmlminTransform);
-  } else {
-    eleventyConfig.setBrowserSyncConfig({ callbacks: { ready: browserSyncReady }});
   }
 
   var pathPrefix = "";
@@ -19,17 +17,6 @@ module.exports = function(eleventyConfig) {
   }
 
 };
-
-function browserSyncReady(err, bs) {
-  bs.addMiddleware("*", (req, res) => {
-    const content_404 = fs.readFileSync('_site/404.html');
-    // Add 404 http status code in request header.
-    res.writeHead(404, { "Content-Type": "text/html; charset=ETF-8" });
-    // Provides the 404 content without redirect.
-    res.write(content_404);
-    res.end();
-  });
-}
 
 function htmlminTransform(content, outputPath) {
   if( outputPath.endsWith(".html") ) {
